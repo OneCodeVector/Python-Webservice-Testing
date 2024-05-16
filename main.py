8f91e0396343d23b47e36cf087711f872e32e681
OneCodeVector <117361107+OneCodeVector@users.noreply.github.com>
2024-05-16 16:25:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests as Requests
+import pandas as Pandas
 
 BASE_URL = 'https://fakestoreapi.com' # Constant
 
 Response = Requests.get(f"{BASE_URL}/products")
-print(Response.status_code)
+JsonContent = Pandas.DataFrame(Response.json())
+
+with open('ResponseData.csv', 'w', newline='') as File:
+    File.write(JsonContent.to_csv())
+    pass
